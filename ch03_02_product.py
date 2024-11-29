66ca70099d018d74e323b097e729e39e38b44a2c
omecity <omeiza.olumoye@gmail.com>
2024-11-29 00:00:05 -0600

    added a recursive product function

---
+++ ch03_02_product.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+'''
+Using the head-tail technique, create a recursive product() function that
+is passed an array of integers and returns the total multiplied product
+of them. This code will be almost identical to the sum() function in this
+chapter. However, note that the base case of an array with just one inte-
+ger returns the integer, and the base case of an empty array returns 1. 
+
+'''

aa405d37c1d57954add2163394f161f353ebdc6c
omecity <omeiza.olumoye@gmail.com>
2024-11-29 09:28:56 -0600

    finished testing the function with different lists

--- ch03_02_product.py
+++ ch03_02_product.py
@@ -8,3 +8,21 @@ chapter. However, note that the base case of an array with just one inte-
 ger returns the integer, and the base case of an empty array returns 1. 
 
 '''
+
+def product(numbers):
+    if len(numbers) == 0:       # BASE CASE
+        return 1
+    else:                       # RECURSIVE CASE
+        head = numbers[0]
+        tail = numbers[1:]
+        return head * product(tail)
+
+
+nums = []
+print('The product of', nums, 'is', product(nums))
+
+nums = [1, 2, 3, 4, 5]
+print('The product of', nums, 'is', product(nums))
+
+nums = [1, 10, 100, 1000]
+print('The product of', nums, 'is', product(nums))

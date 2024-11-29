b1f9624fa6a6968dfe022982c208e07bafbcc03c
omecity <omeiza.olumoye@gmail.com>
2024-11-29 11:21:41 -0600

    added a concat function file

---
+++ ch03_01_concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+'''
+Using the head-tail technique, create a recursive concat() function that
+is passed an array of strings and returns these strings concatenated
+together into a single string. For example, concat(['Hello', 'World'])
+should return HelloWorld.
+'''

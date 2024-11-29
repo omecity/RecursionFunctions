e31eac02e81a6c9a3631688e7528e9c7711b7e83
omecity <omeiza.olumoye@gmail.com>
2024-11-29 11:34:59 -0600

    tested several list of strings on the recursive function that attempts to concatenates the individual strings

--- ch03_01_concat.py
+++ ch03_01_concat.py
@@ -6,3 +6,21 @@ is passed an array of strings and returns these strings concatenated
 together into a single string. For example, concat(['Hello', 'World'])
 should return HelloWorld.
 '''
+
+def concat(str_list):
+    if len(str_list) == 0:       # BASE CASE
+        return ""
+    else:                       # RECURSIVE CASE
+        head = str_list[0]
+        tail = str_list[1:]
+        return head + concat(tail)
+    
+
+str = ["Hello", "World"]
+print('The concatenation of', str,'is', concat(str))
+
+str = ["Dry", "Bones", "Shall", "Live", "Again"]
+print('The concatenation of', str,'is', concat(str))
+
+str = []
+print('The concatenation of', str,'is', concat(str))

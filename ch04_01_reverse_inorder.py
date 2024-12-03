2370aebe44d55881b975201efb6443637a28532f
omecity <omeiza.olumoye@gmail.com>
2024-12-03 08:41:09 -0600

    added a inorder traverse function

--- ch04_01_reverse_inorder.py
+++ ch04_01_reverse_inorder.py
@@ -6,3 +6,13 @@ Create a reverse-inorder search, one that performs an inorder traversal
 but traverses the right child node before the left child node.
 '''
 
+def inorderTraverse(node):
+    if len(node['children']) >= 1:
+        # RECURSIVE CASE
+        inorderTraverse(node['children'][0])    # Traverse the left child.
+        print(node['data'], end=' ')    # Access this node's data.
+        if len(node['children']) >= 2:
+            # RECURSIVE CASE
+            inorderTraverse(node['children'][1])    # Traverse the right child.
+        # BASE CASE
+        return

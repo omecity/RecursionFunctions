21ea5c575c676de3d6c968cecf4eb775c1253f72
omecity <omeiza.olumoye@gmail.com>
2024-12-04 20:50:08 -0600

    created a one_step_deeper file

---
+++ ch04_02_one_step_deeper.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+
+'''
+Create a function that, given a root node as an argument, proceeds to
+make the tree one level deeper by adding one child node to each leaf
+node in the original tree. This function will need to perform a tree
+traversal, detect when it has reached a leaf node, and then add one and
+only one child node to the leaf node. Be sure not to go on and add a
+child node to this new leaf node, as that will eventually cause a stack
+overflow.
+'''
+
